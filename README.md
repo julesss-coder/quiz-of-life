265de08993960b260361345cd8b5b8061cf01c69
julesss-coder <julesss@posteo.de>
2024-01-04 22:03:18 +0100

    Count score of user answers.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # quiz-of-life
+
+The HTML template is taken from NetNinja's course "Modern JavaScript - From Novice to Ninja", URL: https://netninja.dev/courses/modern-javascript-from-novice-to-ninja, lecture 66.
--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+/*
+map of questions and correct answers
+
+anwers = submit form info
+score = 0
+for each answer in answers
+  if answer is correct
+    score++
+
+*/
+const correctAnswers = {
+  q1: "A",
+  q2: "B",
+  q3: "A",
+  q4: "B",
+  q5: "B",
+};
+
+
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let score = 0;
+
+  const formData = new FormData(form);
+  for (const [q, a] of formData.entries()) {
+    if (correctAnswers[q] === a) {
+      score++;
+    }
+  }
+  console.log(`Your score: ${score}/5`);
+});
