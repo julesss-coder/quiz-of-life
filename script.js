7a6b27a45dbdba85c3863dd06ce43c45c3eb85ab
julesss-coder <julesss@posteo.de>
2024-01-04 22:42:12 +0100

    Display score

--- script.js
+++ script.js
@@ -18,16 +18,21 @@ const correctAnswers = {
 
 
 const form = document.querySelector(".quiz-form");
+const quizResult = document.querySelector(".quiz-result");
+const percentageEl = document.querySelector(".percentage");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   let score = 0;
 
   const formData = new FormData(form);
-  for (const [q, a] of formData.entries()) {
-    if (correctAnswers[q] === a) {
+  for (const [question, answer] of formData.entries()) {
+    if (correctAnswers[question] === answer) {
       score++;
     }
   }
-  console.log(`Your score: ${score}/5`);
+
+  let percentage = (score/5 * 100).toFixed(2);
+  quizResult.classList.toggle("d-none");
+  percentageEl.textContent = `${percentage}%`;
 });
