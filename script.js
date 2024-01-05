d6572b41f7462c0df0ce0887b408bf45e1eced50
julesss-coder <julesss@posteo.de>
2024-01-05 21:57:17 +0100

    Animate score. Add random quotes.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/*
-map of questions and correct answers
-
-anwers = submit form info
-score = 0
-for each answer in answers
-  if answer is correct
-    score++
-
-*/
 const correctAnswers = {
   q1: "A",
   q2: "B",
@@ -16,10 +6,22 @@ const correctAnswers = {
   q5: "B",
 };
 
+const quotes = [
+  "What are the three miracles most likely to happen to you today?",
+  "When they say 'Be yourself,' which self do they mean?",
+  "You need regular doses of unreasonable beauty, sublime anomalies, beguiling ephemera, and inexplicable joys.",
+  "Thanks to impermanence, everything is possible. - Thich Nhat Hanh",
+  "Bear in mind that you are the Chosen One, and so is everyone else.", 
+  "Change is good - you go first.",
+  "A moral code will corrupt its users unless it ensures that their primary motivation for being good is because it's fun.",
+  "For best results, break the new rules as skillfully as you break the old rules, and periodically break the way you break all the rules."
+];
+
 
 const form = document.querySelector(".quiz-form");
 const quizResult = document.querySelector(".quiz-result");
 const percentageEl = document.querySelector(".percentage");
+const quote = document.querySelector(".quote");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -32,7 +34,23 @@ form.addEventListener("submit", e => {
     }
   }
 
-  let percentage = (score/5 * 100).toFixed(2);
-  quizResult.classList.toggle("d-none");
-  percentageEl.textContent = `${percentage}%`;
+  let percentage = score/5 * 100;
+  scrollTo(0, 0);
+
+  if (quizResult.classList.contains("d-none") === true) {
+    quizResult.classList.toggle("d-none");
+  }
+
+  let index = Math.floor(Math.random() * 8);
+  quote.textContent = quotes[index];
+
+  let base = 0;
+
+  let timer = setInterval(() => {
+    base++;
+    percentageEl.textContent = `${base}%`;
+    if (base === percentage) {
+      clearInterval(timer);
+    }
+  }, 10);
 });
